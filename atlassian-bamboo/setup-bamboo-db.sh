cfa3c4baa4b5c53c7acea9e1bad5e12f7517f1e7
Michael Johann <mjohann@rails-experts.com>
2015-08-07 20:46:02 +0200

    bamboo db scripts had a problem

--- atlassian-bamboo/setup-bamboo-db.sh
+++ atlassian-bamboo/setup-bamboo-db.sh
@@ -1,35 +1,22 @@
 #!/bin/bash
 echo "******CREATING CROWD & CROWDID DATABASE******"
 gosu postgres postgres --single <<- EOSQL
-  CREATE DATABASE crowd WITH ENCODING 'UNICODE' LC_COLLATE 'C' LC_CTYPE 'C' \
+  CREATE DATABASE bamboo WITH ENCODING 'UNICODE' LC_COLLATE 'C' LC_CTYPE 'C' \
     TEMPLATE template0;
-  CREATE DATABASE crowdid WITH ENCODING 'UNICODE' LC_COLLATE 'C' \
-    LC_CTYPE 'C' TEMPLATE template0;
-  CREATE USER crowd;
-  GRANT ALL PRIVILEGES ON DATABASE crowd to crowd;
-  GRANT ALL PRIVILEGES ON DATABASE crowdid to crowd;
+  CREATE USER bamboo;
+  GRANT ALL PRIVILEGES ON DATABASE bamboo to bamboo;
 EOSQL
 echo ""
 
-{ echo; echo "host crowd crowd 0.0.0.0/0 trust"; } >> "$PGDATA"/pg_hba.conf
-{ echo; echo "host crowdid crowd 0.0.0.0/0 trust"; } >> "$PGDATA"/pg_hba.conf
+{ echo; echo "host bamboo bamboo 0.0.0.0/0 trust"; } >> "$PGDATA"/pg_hba.conf
 
-if [ -r '/tmp/dumps/crowd.dump' ]; then
-    echo "**IMPORTING CROWD DATABASE BACKUP**"
+if [ -r '/tmp/dumps/bamboo.dump' ]; then
+    echo "**IMPORTING BAMBOO DATABASE BACKUP**"
     gosu postgres postgres &
     SERVER=$!; sleep 2
-    gosu postgres psql crowd < /tmp/dumps/crowd.dump
+    gosu postgres psql bamboo < /tmp/dumps/bamboo.dump
     kill $SERVER; wait $SERVER
-    echo "**CROWD DATABASE BACKUP IMPORTED***"
+    echo "**BAMBOO DATABASE BACKUP IMPORTED***"
 fi
 
-if [ -r '/tmp/dumps/crowdid.dump' ]; then
-    echo "**IMPORTING CROWDID DATABASE BACKUP**"
-    gosu postgres postgres &
-    SERVER=$!; sleep 2
-    gosu postgres psql crowdid < /tmp/dumps/crowdid.dump
-    kill $SERVER; wait $SERVER
-    echo "**CROWDID DATABASE BACKUP IMPORTED***"
-fi
-
-echo "******CROWD & CROWDID DATABASE CREATED******"
+echo "******BAMBOO DATABASE CREATED******"
